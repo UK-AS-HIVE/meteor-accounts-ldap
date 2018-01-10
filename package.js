d7227f9f12d77ffee30280bcd3f3412b89733e05
Noah Adler <noah.adler@gmail.com>
2018-01-10 09:28:30 -0500

    Bump version to republish for ABI and versioning changes in 1.4.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   'summary': 'Meteor account login via LDAP',
-  'version': '0.4.1',
+  'version': '0.4.2',
   'git' : 'https://github.com/UK-AS-HIVE/meteor-accounts-ldap',
   'name' : 'hive:accounts-ldap'
 });
@@ -8,11 +8,11 @@ Package.describe({
 Npm.depends({'ldapjs' : '0.7.1', 'connect' : '2.19.3'});
 
 Package.on_use(function (api) {
-  api.use(['routepolicy', 'webapp'], 'server');
-  api.use(['accounts-base', 'underscore'], ['client', 'server']);
+  api.use(['routepolicy@1.0.0', 'webapp@1.0.0'], 'server');
+  api.use(['accounts-base@1.0.0', 'underscore@1.0.0'], ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   //api.use('srp', ['client_functions', 'server']);
-  api.use(['ui', 'templating', 'jquery', 'spacebars'], 'client');
+  api.use(['ui@1.0.0', 'templating@1.0.0', 'jquery@1.0.0', 'spacebars@1.0.0'], 'client');
   api.export('LDAP','server');
   api.add_files([
     'ldap_client.html',
