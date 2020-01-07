4f9b0bb5a879a8f61491aa603a18f79f0e901432
Noah Adler <noah.adler@gmail.com>
2020-01-07 09:38:08 -0500

    Remove double api.imply/api.use of accounts-base to prevent upgrade issues (see https://github.com/meteor/meteor/issues/6039)

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Npm.depends({'ldapjs' : '0.7.1', 'connect' : '2.19.3'});
 Package.on_use(function (api) {
   api.use(['routepolicy@1.0.0', 'webapp@1.0.0'], 'server');
   api.use(['accounts-base@1.0.0', 'underscore@1.0.0'], ['client', 'server']);
-  api.imply('accounts-base', ['client', 'server']);
+  //api.imply('accounts-base', ['client', 'server']);
   //api.use('srp', ['client_functions', 'server']);
   api.use(['ui@1.0.0', 'templating@1.0.0', 'jquery@1.0.0', 'spacebars@1.0.0'], 'client');
   api.export('LDAP','server');
